5b1c5400d259ace13390119d831026f07a382cc8
gglsilva <gabrielchamex@gmail.com>
2022-09-05 15:15:31 -0300

    config: installing pre-commit

--- config/settings/base.py
+++ config/settings/base.py
@@ -9,7 +9,9 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import os
 
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fqf*_d!fcv8_j0+#v#$42o$&s=n$6(s-@jxoo@)n+%4je(x77m'
+SECRET_KEY = config('SECRET_KEY')
 
 ALLOWED_HOSTS = []
 
